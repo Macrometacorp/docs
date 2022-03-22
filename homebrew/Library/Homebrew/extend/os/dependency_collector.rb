58c893369ef85112798b88d6c10266254907f1ec
Nick Parsons <nick@macrometa.com>
2022-03-22 10:27:38 -0600

    Remove unnecessary homebrew

--- homebrew/Library/Homebrew/extend/os/dependency_collector.rb
+++
@@ -1,4 +0,0 @@
-# typed: strict
-# frozen_string_literal: true
-
-require "extend/os/mac/dependency_collector" if OS.mac?
--- homebrew/Library/Homebrew/extend/os/formula.rb
+++
@@ -1,4 +0,0 @@
-# typed: strict
-# frozen_string_literal: true
-
-require "extend/os/linux/formula" if OS.linux?
