99f146f4d2734efda6719a854238933ca213f095
Nick Parsons <nick@macrometa.com>
2022-03-25 14:18:31 -0600

    WIP

---
+++ src/pages/api.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import Head from '@docusaurus/Head';
+import BrowserOnly from '@docusaurus/BrowserOnly';
+
+export default function APIPage() {
+  return (
+    <Layout>
+      <Head>
+        <title>API Reference | Macrometa Docs</title>
+        <meta name="description" content="Macrometa REST API" />
+        <meta name="og:description" content="Macrometa REST API" />
+        {/* Loading styles for elements this way so it doesn't interfere with other styles */}
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/@stoplight/elements/styles.min.css"
+        />
+      </Head>
+      <BrowserOnly
+        fallback={
+          <div className="flex min-h-screen w-full items-center justify-center">
+            <div
+              className="h-10 w-10 animate-spin rounded-full border-l border-t-2 border-primary"
+              aria-label="Loading..."
+            ></div>
+          </div>
+        }
+      >
+        {() => {
+          // eslint-disable-next-line no-undef
+          const { API } = require('@stoplight/elements');
+          return (
+            <>
+              {/* https://api-gdn.paas.macrometa.io/_admin/api/swagger.json */}
+              <API
+                apiDescriptionUrl="/swagger.json"
+                router="hash"
+                basePath="/"
+                layout="sidebar"
+                hideMocking
+                hideInternal
+              />
+            </>
+          );
+        }}
+      </BrowserOnly>
+    </Layout>
+  );
+}
