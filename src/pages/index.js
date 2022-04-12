553969c7d1cfd44bac9bcf70071adb47dfd91cca
Nick Parsons <nick@macrometa.com>
2022-04-12 10:38:35 -0600

    Add OG image and update title/description

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,13 +11,11 @@ export default function Homepage() {
       <Head>
         <meta title="Macrometa Docs" />
         <meta property="og:title" content="Macrometa Docs" />
+        <meta property="og:url" content="https://macrometa.com/docs" />
+        <meta property="og:image" content="https://macrometa.com/docs/assets/images/map.png" />
         <meta
           property="og:description"
-          description="Description will go here"
-        />
-        <meta
-          property="description"
-          content="Description will go here"
+          description="Powering the next generation of apps and APIs. Build performant apps on the edge with our lightning-fast, stateful serverless global data platform"
         />
         <link rel="canonical" href="https://macrometa.com/docs" />
       </Head>
