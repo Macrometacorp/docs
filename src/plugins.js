6c83507a39de5011350b9653d904c375e5acf609
diana-macrometa <diana.payton@macrometa.com>
2022-11-03 11:47:30 -0700

    Update plugins.js

--- src/plugins.js
+++ src/plugins.js
@@ -19,7 +19,7 @@ const redirectsPlugin = [
       },
       {
         from: '/essentials/geofabrics',
-        to: '/geofabrics/index'
+        to: '/geofabrics/'
       },
       {
         from: '/essentials/troubleshooting',
