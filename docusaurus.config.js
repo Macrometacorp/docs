f5312e97f8d889fcef2b0024f6c81bf7f390fccb
Dustin Larimer <180438+dustinlarimer@users.noreply.github.com>
2023-01-11 09:56:05 -0800

    Revert config change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   title: 'Macrometa',
   tagline: 'Macrometa GDN Documentation',
   url: host,
-  baseUrl: isDev ? '/' : '/docs/',
+  baseUrl: isDev ? '/docs/' : '/docs/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
