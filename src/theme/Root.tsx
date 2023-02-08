d9da82bec54fe56fe0457af4f3545f709294bc7d
Dustin <180438+dustinlarimer@users.noreply.github.com>
2023-02-07 21:32:10 -0800

    Track web vitals (#641)

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Analytics } from '@vercel/analytics/react';
+import { reportWebVitals } from '../vitals';
 
 export default function Root({ children }) {
   return (
@@ -9,3 +10,5 @@ export default function Root({ children }) {
     </>
   );
 }
+
+reportWebVitals();
