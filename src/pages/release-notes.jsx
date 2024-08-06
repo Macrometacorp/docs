adfbcd6d3e36f0b6304d54693f84da03578499c2
maryann-macrometa <maryann.agofure@macrometa.com>
2024-08-06 18:49:41 +0100

    added content, few changes to ui

--- src/pages/release-notes.jsx
+++ src/pages/release-notes.jsx
@@ -7,21 +7,22 @@ const App = () => {
   const notes = [
     {
       id: 1, date: "August 26, 2024", tag: "PhotonIQ", Product: "Prerender v1.2.1", whatsnew: "What's New",
-      whatsNewContents: [ "lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur.", "Lorem ipsum dolor sit amet consectetur.", "Lorem ipsum dolor sit amet consectetur."],
-      Improvement: "Improvement", improvementContents: [ "Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur.", "Lorem ipsum dolor sit amet consectetur.", "Lorem ipsum dolor sit amet consectetur." ],
-      bugFix: "Bug Fixes", bugFixContents: [ "Lorem ipsum dolor sit amet consectetur.", "Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur.", "Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur.",
-      ],
+      whatsNewContents: [ "Sync service for better connection and fault tolerance.", "Auth service for role-based access control.", "Editable GUI for better configuration management.", "Added Follow Redirect settings to manage redirect behavior."],
+      Improvement: "Improvement", improvementContents: [ "Reduced load on GDN server aggregating and serving metrics every minute instead of pushing per request.", "Added new URL tags to the URL block list.", "Removed the entire cache purge feature." ],
+      bugFix: "Bug Fixes", bugFixContents: [],
     },
 
     {
       id: 2, date: "August 26, 2024", tag: "GDN", Product: "Virtual Waiting Rooms 1.2.3 ", whatsnew: "What's New",
-      whatsNewContents: [ "lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur.", "Lorem ipsum dolor sit amet consectetur.", "Lorem ipsum dolor sit amet consectetur." ], 
-      Improvement: "Improvement", improvementContents: [ "lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur.", "Lorem ipsum dolor sit amet consectetur.", "Lorem ipsum dolor sit amet consectetur." ],
-       bugFix: "Bug Fixes", bugFixContents: [ "lorem ipsum dolor sit amet consectetur.", "Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur.", "Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur."]
-    },
+      whatsNewContents: [ "Create collections with strong consistency.", "Improvements to the web console for better management.", "New AWS S4 target for connection", "New API endpoint to Stream workers", "New Global API endpoint to Fabrics" ], 
+      Improvement: "Improvement", improvementContents: [ "New Group ID API Endpoints", "Enhanced Error Handling for Stream Workers", "Stream workers support for all collection types.", "Addition of Akamai EdgeWorkers code bundles" ],
+       bugFix: "Bug Fixes", bugFixContents: [ "Corrected field order and capitalization on the Invite User signup page.", "Delete subscriptions only at stream worker deletion, not unpublish.", "Fixed Regex for http.status.code in http-call-response function."]},
   ];
 
-  const filteredNotes = selectedTag === "All" ? notes : notes.filter((note) => note.tag === selectedTag);
+  const filteredNotes = selectedTag === "All" 
+  ? notes 
+  : notes.filter((note) => note.tag === selectedTag);
+  
 
   return (
     <Layout>
@@ -33,22 +34,22 @@ const App = () => {
         <ul className="flex items-center gap-3">
 
           <li className={`list-none py-[4px] px-[8px] text-[14px] rounded-md cursor-pointer ${
-              selectedTag === "All"  ? "bg-[#1a2b3b] text-white"  : "bg-[#edf0f2] text-[#687887]" }`}
+              selectedTag === "All"  ? "bg-[#6767E5] text-white"  : "bg-[#edf0f2] text-[#687887]" }`}
               onClick={() => setSelectedTag("All")}> All </li>
 
           <li className={`list-none py-[4px] px-[8px] text-[14px] rounded-md cursor-pointer ${
-               selectedTag === "PhotonIQ" ? "bg-[#1a2b3b] text-white" : "bg-[#edf0f2] text-[#687887]"}`}
+               selectedTag === "PhotonIQ" ? "bg-[#6767E5] text-white" : "bg-[#edf0f2] text-[#687887]"}`}
                onClick={() => setSelectedTag("PhotonIQ")} > PhotonIQ </li>
 
           <li className={`list-none py-[4px] px-[8px] text-[14px] rounded-md cursor-pointer ${
-              selectedTag === "GDN" ? "bg-[#1a2b3b] text-white" : "bg-[#edf0f2] text-[#687887]" }`}
+              selectedTag === "GDN" ? "bg-[#6767E5] text-white" : "bg-[#edf0f2] text-[#687887]" }`}
             onClick={() => setSelectedTag("GDN")}> GDN </li>
         </ul>
       </header>
       <div className="mt-6">
         <div className="pb-20 md:pb-24 w-full max-w-screen-xl xl:mx-auto">
           <div className="relative min-h-[600px] w-full">
-            <div className="absolute bottom-0 left-0 top-2 w-[2px] bg-gray-100 md:left-[25%] after:absolute after:inset-x-0 after:bottom-0 after:h-48 after:bg-gradient-to-b after:from-gray-100 after:to-white"></div>
+            <div className="absolute bottom-0 left-0 top-2 w-[2px] bg-indigo-200 md:left-[25%] after:absolute after:inset-x-0 after:bottom-0 after:h-48 after:bg-gradient-to-b after:from-gray-100 after:to-white"></div>
             {filteredNotes.map((note) => {
               let { id, date, tag, Product, whatsnew, whatsNewContents, Improvement, improvementContents, bugFix, bugFixContents } = note;
               return (
@@ -57,7 +58,7 @@ const App = () => {
                     <div className="text-gray-600 md:mb-1 whitespace-nowrap">
                       {date}
                     </div>
-                    <span className="bg-gray-100 w-fit flex-initial rounded-md px-1.5 py-0.5 text-sm text-gray-600">
+                    <span className="bg-indigo-200 w-fit flex-initial rounded-md px-1.5 py-0.5 text-sm text-gray-600">
                       {tag}
                     </span>
                     <div className="absolute left-[-31px] top-1.5 h-4 w-4 rounded-full border-[2px] border-gray-200 bg-white md:left-auto md:right-[-9px]"></div>
