2da9e183fe7ce87e602ac427727c337f7aac8d2d
funke-32 <funke.olasupo@macrometa.com>
2024-12-31 14:15:54 +0100

    Update release-notes.jsx

--- src/pages/release-notes.jsx
+++ src/pages/release-notes.jsx
@@ -8,7 +8,7 @@ const App = () => {
     {
       id: 4, date: "December 30, 2024", tag: "Workspaces", Product: "Workspaces v1.1.0",
       whatsnew: "What's new", whatsNewContents: ["User and host management in VDI: Workspace admins can now manage users and hosts effectively to optimize resource allocation. The new Users & Hosts dashboard enables admins to assign or unassign users to specific host, filter hosts based on assignment status or host type, and more. "],
-      Improvement: "Improvement", improvementContents: [],
+      Improvement: "Improvement", improvementContents: ["Speed: Indexes have been added to sessions and session_assignments to improve performance, ensuring faster and more responsive filtering on the admin page."],
       bugFix: "Bug fixes", bugFixContents: [],
       blogPostTitle:"Workspaces Admin guide" , blogPostLink: "https://www.macrometa.com/docs/workspaces/workspace-management",
 
